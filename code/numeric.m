2ea51cc6033e691b624e86ec3c009b074e43c44c
David Bascelli <davidb996@gmail.com>
2019-02-24 15:41:27 -0500

    Fixed corner cases

--- code/numeric.m
+++ code/numeric.m
@@ -12,7 +12,53 @@ function V = numeric(nx, ny, cMap, bc_left, bc_right, bc_top, bc_bottom)
             nym = map(i,j-1, nx, ny);
             nyp = map(i,j+1, nx, ny);
 
-            if (i == 1) % Left Side
+            if (i == 1 && j == 1)
+                if (bc_left == Inf)
+                    rxp = (cMap(i,j) + cMap(i+1,j))/2;
+                    ryp = (cMap(i,j) + cMap(i,j+1))/2;
+
+                    G(n,n)   = -(rxp+ryp);
+                    G(n,nxp) =  rxp;
+                    G(n,nyp) =  ryp;
+                else
+                    G(n,n) = 1;
+                    B(n) = bc_left;
+                end
+            elseif (i == 1 && j == ny)
+                if (bc_left == Inf)
+                    rxp = (cMap(i,j) + cMap(i+1,j))/2;
+                    rym = (cMap(i,j) + cMap(i,j-1))/2;
+
+                    G(n,n)   = -(rxp+rym);
+                    G(n,nxp) =  rxp;
+                    G(n,nym) =  rym;
+                else
+                    G(n,n) = 1;
+                    B(n) = bc_left;
+                end
+            elseif i == nx && j == 1 % Right side
+                if (bc_right == Inf)
+                    rxm = (cMap(i,j) + cMap(i-1,j))/2;
+                    ryp = (cMap(i,j) + cMap(i,j+1))/2;
+                    G(n,n)   = -(rxm+ryp);
+                    G(n,nxm) =  rxm;
+                    G(n,nyp) =  ryp;
+                else
+                    G(n,n) = 1;
+                    B(n) = bc_right;
+                end
+            elseif i == nx && j == ny % Right side
+                if (bc_right == Inf)
+                    rxm = (cMap(i,j) + cMap(i-1,j))/2;
+                    rym = (cMap(i,j) + cMap(i,j-1))/2;
+                    G(n,n)   = -(rxm+rym);
+                    G(n,nxm) =  rxm;
+                    G(n,nym) =  rym;
+                else
+                    G(n,n) = 1;
+                    B(n) = bc_right;
+                end
+            elseif (i == 1) % Left Side
                 if (bc_left == Inf)
                     rxp = (cMap(i,j) + cMap(i+1,j))/2;
                     rym = (cMap(i,j) + cMap(i,j-1))/2;
